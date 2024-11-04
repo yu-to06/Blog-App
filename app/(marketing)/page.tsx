896e396e2ff3128106c7fa218b97f8b22db3b723
yu-to06 <shun6.u@gmail.com>
2024-11-04 23:11:32 +0900

    create types and config

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,15 +1,38 @@
+import {buttonVariants} from "@/components/ui/button";
 import Link from "next/link";
+import {cn} from "@/lib/utils";
+import {siteConfig} from "@/config/site";
 
 const IndexPage = () => {
   return (
     <>
-      <section>
-        <div className='pt-6 pb-8 md:pt-10 md:pb-12 lg:py-32'>
-          <Link href={"/"}>Xをフォローする</Link>
-          <h1>Post Writer</h1>
-          <p>
+      <section className='pt-6 pb-8 md:pt-10 md:pb-12 lg:py-32'>
+        <div className='container text-center flex flex-col items-center gap-4 max-w-[64rem]'>
+          <Link
+            href={"siteConfig.links.x"}
+            className='bg-muted px-4 py-1.5 rounded-2xl font-medium text-sm'
+          >
+            Xをフォローする
+          </Link>
+          <h1 className='font-extrabold text-3xl sm:text-5xl md:text-6xl lg:text-7xl'>
+            Post Writer
+          </h1>
+          <p className='text-muted-foreground leading-normal  sm:text-xl max-w-[42rem]'>
             このアプリケーションはNext.js13を使用して作成されたものです。ユーザーは投稿をポストすることができ、マークダウン形式でブログを投稿することもできます。
           </p>
+          <div className='space-x-4'>
+            <Link href={"/login"} className={cn(buttonVariants({size: "lg"}))}>
+              Let's Start!
+            </Link>
+            <Link
+              href={"siteConfig.links.Github"}
+              className={cn(buttonVariants({size: "lg", variant: "outline"}))}
+              target='_blank'
+              rel='noreferrer'
+            >
+              Github
+            </Link>
+          </div>
         </div>
       </section>
     </>
---
+++ config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig : SiteConfig={
+ name:"Post Writer",
+ description:"",
+ url:"http://localhost:3000",
+ ogImage:"",
+ links:{
+    x:"http://localhost:3000",
+    github:"https://github.com/yu-to06/Blog-App"
+ },
+}
