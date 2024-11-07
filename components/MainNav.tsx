231661935008f49d469d493625a52b3284b2472d
yu-to06 <shun6.u@gmail.com>
2024-11-07 23:16:52 +0900

    add MainNav

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import {NavItem} from "@/types";
 import Link from "next/link";
-import {ReactNode} from "react";
+import {ReactNode, useState} from "react";
+import MobileNav from "./MobileNav";
 
 interface MainNavProps {
-  items?: NavItem[];
+  items: NavItem[];
   children?: ReactNode;
 }
 
 export default function MainNav({items}: MainNavProps) {
+  const [showMobileMenu, setshowMobileMenu] = useState<boolean>(false);
   return (
     <div className='flex items-center md:gap-10'>
       <Link href={"/"} className='hidden md:flex items-center space-x-2'>
@@ -24,6 +28,15 @@ export default function MainNav({items}: MainNavProps) {
           </Link>
         ))}
       </nav>
+
+      {/* ハンバーガー */}
+      <button
+        className='md:hidden'
+        onClick={() => setshowMobileMenu(!showMobileMenu)}
+      >
+        <span>Menu</span>
+      </button>
+      {showMobileMenu && <MobileNav items={items} />}
     </div>
   );
 }
---
+++ components/MobileNav.tsx
@@ -0,0 +1,26 @@
+import {siteConfig} from "@/config/site";
+import {NavItem} from "@/types";
+import Link from "next/link";
+
+interface MobileNavProps {
+  items: NavItem[];
+}
+
+export default function MobileNav({items}: MobileNavProps) {
+  return (
+    <div className='fixed top-16 inset-0 z-50 p-6 shadow-md md:hidden animate-in slide-in-from-bottom-80 '>
+      <div className='grid gap-6 bg-popover p-4 text-popover-foreground shadow-md'>
+        <Link href={"/"} className='font-bold'>
+          {siteConfig.name}
+        </Link>
+        <nav className='text-sm flex gap-4'>
+          {items.map((item, index) => (
+            <Link key={index} href={item.href}>
+              {item.title}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </div>
+  );
+}
