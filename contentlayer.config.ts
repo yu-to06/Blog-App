a0e06f9b75579ca9fffcc4f2acbe7f6fe617de5a
yu-to06 <shun6.u@gmail.com>
2024-12-01 22:50:04 +0900

    fixed tailwind

---
+++ contentlayer.config.ts
@@ -0,0 +1,24 @@
+import {defineDocumentType, makeSource} from "contentlayer/source-files";
+
+export const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `blog/**/*.mdx`,
+  fields: {
+    title: {type: "string", required: true},
+    description: {type: "string"},
+    date: {type: "date", required: true},
+    published: {type: "boolean", default: true},
+    images: {type: "string", required: true},
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => `${doc._raw.flattenedPath}`,
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: "./content",
+  documentTypes: [Post],
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
     darkMode: ["class"],
     content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
@@ -50,13 +57,8 @@ export default {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
   		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
